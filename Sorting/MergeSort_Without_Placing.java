61e5c3e0f16007e68e6747ccff302d5873444384
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-01-19 11:43:42 +0530

    Create MergeSort_Without_Placing.java
    Merge Sort using the Array copy method

---
+++ Sorting/MergeSort_Without_Placing.java
@@ -0,0 +1,55 @@
+public class Sort {
+  public static void main(string args) {
+    int[] arr = {1, 2, 4, 5, 3};
+    mergeSort(arr);
+  }
+
+public int[] mergeSort(int[] arr) {
+
+  if(arr.length == 1) {
+    return;
+  }
+  
+  int mid = arr.length / 2;
+
+  int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+  int[] right = mergeSort(Arrays.copyOfRange(arr, mid,arr.length));
+
+  mix(left, right);
+}
+
+public int[] mix(int[] l, int[] r) {
+  int[] mix = new int[l.length + r.length)];
+
+  int i, j, k = 0;
+
+  while(i < l.length && j < r.length) {
+
+    if(l[i] < r[j]) {
+      l[i] = mix[k];
+      i++;
+    } else {
+      r[j] = mix[k];
+      j++;
+    }
+    k++;
+  }
+
+  // It may be possible that 1 among the array is not completely traversed , so that next process ensures the merge completely.
+  //Copy the remianing elements that are not not tbe compared.
+  
+  while(i < l.length) {
+    l[i] = mix[k];
+    i++;
+    k++;
+  }
+
+  while(j < r.length) {
+    r[j] = mix[k];
+    j++;
+    k++;
+  }
+
+  return mix;
+  }
+}
