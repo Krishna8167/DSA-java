40ab2bf4890af10806ffb82a7f76d8efbe5049f2
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2026-01-19 11:20:40 +0530

    Create RecursionBubbleSort.java
    Recursive approach for the Bubble Sort algorithm

---
+++ Sorting/RecursionBubbleSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] arr = {22, 19, 15, 3, 5};
+		
+		BubbleSort(arr, arr.length - 1, 0);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
+	static void BubbleSort(int[] arr, int r , int c) {
+	    
+	    if (r == 0) {
+	        return;
+	    }
+	   
+	   if (c < r) {
+	       
+	       if(arr[c] > arr[c+1]) {
+	           swap(arr, c, c+1);
+	       }
+	       
+	       BubbleSort(arr, r, c+1);
+	   } else {
+	       BubbleSort(arr, r-1, 0);
+	   }
+	}
+	
+	static void swap(int[] arr, int first, int second) {
+	    
+	    int temp = arr[first];
+	    arr[first] = arr[second];
+	    arr[second] = temp;
+	    
+	}
+	
+	
+}
+
+
+// Problem solving using the down facing triangle concept of row and column.
